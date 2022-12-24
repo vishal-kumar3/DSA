9a6e9a8a9eba4801bce292ccb588230e530d7897
vishal-kumar3 <121259738+vishal-kumar3@users.noreply.github.com>
2022-12-24 05:56:01 +0000

    Stack.py me comments add kiye.

--- Stack.py
+++ Stack.py
@@ -1,11 +1,21 @@
+# Stack- Data Structure
+
+# List hota h jo FILO ko follow krta h
+# FIRST IN LAST OUT
+# ________________
+# | 5, 6, 7, 2, 10  aisa design hota h stack ka
+# ----------------
+
+# One way movement hota h, jo pehle jayega vo last me bahar ayega
 
 
 class Stack:
     def __init__(self):
         self.stack = []
 
-    # To add element to stack
+    # To add element(data) to stack
     def push(self, data):
+        # if not present in stack
         if data not in self.stack:
             self.stack.append(data)
             return True
@@ -14,12 +24,14 @@ class Stack:
 
     # To peek Top element
     def peek(self):
+        # Agar stack khali na ho
         if len(self.stack) > 0:
             return self.stack[-1]
         print('No Element in Stack')
 
     # to remove the top element
     def pop(self):
+        # Agar stack khali na ho
         if len(self.stack) > 0:
             return self.stack.pop()
         print('No Element in Stack')
